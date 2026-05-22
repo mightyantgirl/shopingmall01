b9450d4971fade40852f4f0b4ea55e63cab1846f
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2026-05-22 19:59:26 +0900

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/shop-card/",
+  base: "/shopingmall01/",
   plugins: [react()],
 });
